1beae9db611076053df53100d7df9ff69e9737e6
Michael Derfler <derfler.michael@gamil.com>
2018-01-15 23:13:16 +0100

    remove bin files

--- src/schema_parser/bin/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SchemaParser_1 = require("./src/SchemaParser");
-var parser = new SchemaParser_1.SchemaParser();
-parser.parse('./schemas/Opc.Ua.Types.bsd');
-//# sourceMappingURL=index.js.map
