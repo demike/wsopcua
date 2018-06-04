67a023fc652704ae354cfc1f52d978492707da52
Derfler Michael <Michael.Derfler@engel.at>
2018-06-04 17:15:06 +0200

    add browse service

---
+++ src/service-browse/index.ts
@@ -0,0 +1,43 @@
+"use strict";
+/**
+ * @module services.browse
+ */
+
+    /**
+     * @class BrowseDirection
+     */
+    export {BrowseDirection} from "../generated/BrowseDirection";
+    /**
+     * @class ReferenceDescription
+     */
+    export {ReferenceDescription} from "../generated/ReferenceDescription";
+    /**
+     * @class BrowseResult
+     */
+    export {BrowseResult} from "../generated/BrowseResult";
+    /**
+     * @class BrowseDescription
+     */
+    export {BrowseDescription} from "../generated/BrowseDescription";
+    /**
+     * @class BrowseRequest
+     */
+    export {BrowseRequest} from "../generated/BrowseRequest";
+    /**
+     * @class BrowseResponse
+     */
+    export {BrowseResponse} from "../generated/BrowseResponse";
+    // export {NodeClass} from "").NodeClass;
+
+    /**
+     * @class BrowseNextRequest
+     */
+    export {BrowseNextRequest} from "../generated/BrowseNextRequest";
+    /**
+     * @class BrowseNextResponse
+     */
+    export {BrowseNextResponse} from "../generated/BrowseNextResponse";
+
+    //export {ViewDescription} from "../generated/ViewDescription";
+
+
