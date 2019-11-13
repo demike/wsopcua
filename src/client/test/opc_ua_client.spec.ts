8c5123d882a06d53634b74b9572b5f0d6d62da64
29831.derf.m <michael.derfler@engel.at>
2019-11-13 08:35:14 +0100

    test(): connection failure

--- src/client/test/opc_ua_client.spec.ts
+++ src/client/test/opc_ua_client.spec.ts
@@ -1,6 +1,7 @@
 'use strict';
 import {OPCUAClient} from '../opcua_client';
 import { ClientSecureChannelLayer } from '../../secure-channel/client_secure_channel_layer';
+import { timeout } from 'async';
 
 
 
@@ -16,4 +17,44 @@ describe('OPCUA Client', function() {
         const secLayer = new ClientSecureChannelLayer({});
     });
 
+    describe('issue 696: https://github.com/node-opcua/node-opcua/issues/696', function() {
+        let setIntervalCalls = 0;
+        const realSetInterval = window.setInterval;
+        let clearIntervalCalls = 0;
+        const realClearInterval = window.clearInterval;
+
+        beforeEach(() => {
+            window.setInterval = (...args) => {
+                setIntervalCalls++;
+                return (<any>realSetInterval)(...args);
+            };
+            window.clearInterval = (...args) => {
+                clearIntervalCalls++;
+                return realClearInterval(...args);
+            };
+        });
+        afterEach(() => {
+            window.setInterval = realSetInterval;
+            window.clearInterval = realClearInterval;
+        });
+        it('should not leak interval if connection failed', async () => {
+
+            async function test() {
+                try {
+                    const client = new OPCUAClient({ connectionStrategy: { maxRetry: 0 } });
+                    await client.connectP('invalid-proto://test-host');
+                } catch (err) {
+                    console.log(err.message);
+                    throw err;
+                }
+            }
+            await expectAsync(test()).toBeRejectedWith(/The connection has been rejected/);
+            console.log(`setIntervalCalls ${setIntervalCalls} vs. clearIntervalCalls ${clearIntervalCalls}`);
+
+            expect(setIntervalCalls).toEqual(clearIntervalCalls);
+        });
+    });
+
 });
+
+
