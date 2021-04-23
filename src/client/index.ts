4b60a006116babe4e39d6050fecb697f66889b31
demike <github+dev.mderf@gmail.com>
2021-04-23 09:00:11 +0200

    fix: missing exports + packaging structure

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,11 +2,13 @@ export * from './client_base';
 export * from './client_publish_engine';
 export * from './client_session_keepalive_manager';
 export * from './client_session';
+export * from './client_utils';
 
 export * from '../common/client_options';
 export * from './opcua_client';
+
 export * from './MonitoredItem';
+export * from './MonitoredItemBase';
+export * from './MonitoredItemGroup';
 
 export * from './ClientSubscription';
-
-export * from './client_utils';
--- tools/generate-alias.js
+++ tools/generate-alias.js
@@ -6,6 +6,7 @@ const path = require('path');
 const packageJson = require('../package.json');
 
 const DIST_PATH = path.resolve(__dirname, `../dist`);
+const TYPES_PATH = path.resolve(__dirname, `../dist/_types`);
 
 const insertPackageJsonRecursively = function (dirPath) {
   files = fs.readdirSync(dirPath);
@@ -14,30 +15,36 @@ const insertPackageJsonRecursively = function (dirPath) {
     if (fs.statSync(dirPath + '/' + file).isDirectory()) {
       insertPackageJsonRecursively(dirPath + '/' + file);
     } else {
-      if (file === 'index.d.ts' && dirPath !== DIST_PATH) {
-        const pkgManifest = createPackageManifest(dirPath);
-        fs.writeJSON(path.resolve(__dirname, `${dirPath}/package.json`), pkgManifest, {
-          spaces: 2,
-        });
+      if (file === 'index.d.ts' && dirPath !== TYPES_PATH) {
+        // create package.json files skipping the root package.json
+        createPackageManifest(dirPath);
       }
     }
   });
 };
 
 function createPackageManifest(barrelPath) {
-  let relPath = toUnixPath(path.relative(DIST_PATH, barrelPath));
+  let relPath = toUnixPath(path.relative(TYPES_PATH, barrelPath));
   console.log(relPath);
-  let relPathReverse = toUnixPath(path.relative(barrelPath, DIST_PATH));
+  let relPathReverse = toUnixPath(path.relative(barrelPath, TYPES_PATH));
   console.log(relPathReverse);
   let alias = relPath;
 
   const pkgManifest = {
     name: `${packageJson.name}/${relPath}`,
-    types: `${relPathReverse}/${relPath}/index.d.ts`,
+    types: `${relPathReverse}/_types/${relPath}/index.d.ts`,
     main: `${relPathReverse}/_cjs/${relPath}/index.js`,
     module: `${relPathReverse}/_esm/${relPath}/index.js`,
     es2015: `${relPathReverse}/_esm/${relPath}/index.js`,
   };
+
+  const packageJsonPath = path.resolve(DIST_PATH, `${relPath}/package.json`);
+  console.log(packageJsonPath);
+
+  fs.outputJSONSync(path.resolve(DIST_PATH, packageJsonPath), pkgManifest, {
+    spaces: 2,
+  });
+
   return pkgManifest;
 }
 
@@ -45,4 +52,4 @@ function toUnixPath(p) {
   return p.split(path.sep).join(path.posix.sep);
 }
 
-insertPackageJsonRecursively(DIST_PATH);
+insertPackageJsonRecursively(TYPES_PATH);
