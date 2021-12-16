c5c677d983a3d8486aa85c2a189ba3cdfa86bcb1
demike <github+dev.mderf@gmail.com>
2021-12-16 15:46:02 +0100

    fix: makeApplicationURN

--- src/common/applicationurn.spec.ts
+++ src/common/applicationurn.spec.ts
@@ -18,15 +18,22 @@ describe('makeApplicationUrn', function () {
     expect(urn).toEqual('urn:fa63b28b1d158ba8:theapp');
   });
 
-  it('should assert on long hostname and applicationname resulting in urn length > 64 ', async () => {
+  it('should truncate on long hostname and applicationname that would result in urn length > 64 ', async () => {
     const hostname = 'https://some.really.really.really.long.name.here.test.com/opcua/test/';
     const appname = 'theapp.with.an.extremly.extremly.extremly.long.name';
 
-    try {
-      const urn = await makeApplicationUrn(hostname, appname);
-      fail('should have asserted');
-    } catch (e) {
-      expect(e).toBeInstanceOf(AssertionError);
-    }
+    const urn = await makeApplicationUrn(hostname, appname);
+
+    expect(urn.length).toBe(64);
+    expect(urn).toEqual('urn:fa63b28b1d158ba8:theapp.with.an.extremly.extremly.extremly.l');
+  });
+
+  it('should create an urn from a long host name without crypto.subtle (http)', async () => {
+    const hostname = 'https://some.really.really.really.long.name.here.test.com/opcua/test/';
+    const appname = 'theapp';
+    spyOnProperty(window, 'crypto', 'get').and.returnValue(undefined);
+
+    const urn = await makeApplicationUrn(hostname, appname);
+    expect(urn).toEqual('urn:39eeb0c7:theapp');
   });
 });
--- src/common/applicationurn.ts
+++ src/common/applicationurn.ts
@@ -1,9 +1,7 @@
 'use strict';
-import { assert } from '../assert';
-
-const crypto: Crypto = window.crypto || (<any>window).msCrypto; // for IE 11//require("crypto");
 
 export async function makeApplicationUrn(hostname: string, suffix: string): Promise<string> {
+  const crypto: Crypto = window.crypto || (<any>window).msCrypto; // for IE 11//require("crypto");
   // beware : Openssl doesn't support urn with length greater than 64 !!
   //          sometimes hostname length could be too long ...
   // application urn length must not exceed 64 car. to comply with openssl
@@ -12,17 +10,23 @@ export async function makeApplicationUrn(hostname: string, suffix: string): Prom
   if (hostname.length + 7 + suffix.length >= 64) {
     // we need to reduce the applicationUrn side => let's take
     // a portion of the hostname hash.
-    const hashBuffer = await crypto.subtle.digest({ name: 'SHA-1' }, str2arraybuffer(hostname));
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    hostnameHash = hashArray
-      .map((b) => b.toString(16).padStart(2, '0'))
-      .join('')
-      .substring(0, 16);
+    if (crypto && crypto.subtle) {
+      const hashBuffer = await crypto.subtle.digest({ name: 'SHA-1' }, str2arraybuffer(hostname));
+      const hashArray = Array.from(new Uint8Array(hashBuffer));
+      hostnameHash = hashArray
+        .map((b) => b.toString(16).padStart(2, '0'))
+        .join('')
+        .substring(0, 16);
+    } else {
+      // no crypto.subtle (maybe no https)
+      hostnameHash = hashCode(hostname).toString(16).substring(0, 16);
+    }
   }
 
   const applicationUrn = 'urn:' + hostnameHash + ':' + suffix;
-  assert(applicationUrn.length <= 64);
-  return applicationUrn;
+
+  // ensure max 64 characters
+  return applicationUrn.substring(0, 64);
 }
 
 function str2arraybuffer(str: String): ArrayBuffer {
@@ -31,4 +35,16 @@ function str2arraybuffer(str: String): ArrayBuffer {
     array[i] = str.charCodeAt(i);
   }
   return array.buffer;
+}
+
+// equivalent of java's String.hashCode
+function hashCode(str: string) {
+  let hash = 0;
+  if (str.length == 0) return hash;
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
 }
